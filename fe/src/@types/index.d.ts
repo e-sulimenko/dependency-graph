bcceed14a826da5019043e9a6daf5151c3304f8a
Sulimenko Erik <erik.sulimenko@gmail.com>
2023-09-27 11:25:37 +0300

    refactor parse data

--- fe/src/@types/index.d.ts
+++ fe/src/@types/index.d.ts
@@ -1,4 +1,12 @@
-import type { NodeDefinition, EdgeDefinition } from 'cytoscape';
+import type { EdgeDefinition, NodeDataDefinition } from 'cytoscape';
+
+export interface NodeData extends NodeDataDefinition {
+  type: 'module' | 'specifier' | 'tmp'
+}
+
+type NodeDefinition = {
+  data: NodeData;
+}
 
 export type ParsedData = {
   nodes: NodeDefinition[];
--- fe/src/cy/parse-data.ts
+++ fe/src/cy/parse-data.ts
@@ -1,64 +1,75 @@
-import { ParsedData } from '../@types';
+import type { EdgeDefinition } from 'cytoscape';
+import type { ParsedData, NodeData, NodeDefinition } from '../@types';
 
 
 const EDGE_PATH_SEPARATOR = '->'
 const NODE_PATH_SEPARATOR = '|'
 
+const createNode = (id: string, name: string, type: NodeData['type']): NodeDefinition => ({
+  data: {
+    id,
+    name,
+    type,
+  },
+});
+
+const createEdge = (id: string): EdgeDefinition => {
+  const [source, target] = id.split(EDGE_PATH_SEPARATOR);
+  return ({
+    data: {
+      id,
+      source,
+      target,
+    }
+  });
+}
+
 export const parseData = (): ParsedData => {
-  const componentsSet = new Set<string>();
-  const specifierSet = new Set<string>();
-  const edgesSet = new Set<string>();
+  const nodesDict: Record<string, ParsedData['nodes'][0]> = {}
+  const edgeDict: Record<string, ParsedData['edges'][0]> = {}
 
   initialData.forEach((depNode) => {
-    componentsSet.add(depNode.parentPath);
+    nodesDict[depNode.parentPath] = createNode(
+      depNode.parentPath,
+      depNode.parentPath.split('/').at(-1) ?? depNode.parentPath,
+      'module',
+    );
 
     if (depNode.specifiers.length === 0) {
-      componentsSet.add(depNode.childPath);
-      edgesSet.add(depNode.parentPath + EDGE_PATH_SEPARATOR + depNode.childPath);
+      nodesDict[depNode.childPath] = createNode(
+        depNode.childPath,
+        depNode.childPath.split('/').at(-1) ?? depNode.childPath,
+        'tmp',
+      );
+      const edgeId = depNode.parentPath + EDGE_PATH_SEPARATOR + depNode.childPath;
+      edgeDict[edgeId] = createEdge(edgeId);
     }
 
     depNode.specifiers.forEach((specifier) => {
       if (specifier.type === 'ImportDefaultSpecifier') {
-        componentsSet.add(depNode.childPath);
-        edgesSet.add(depNode.parentPath + EDGE_PATH_SEPARATOR + depNode.childPath);
+        nodesDict[depNode.childPath] = createNode(
+          depNode.childPath,
+          depNode.childPath.split('/').at(-1) ?? depNode.childPath,
+          'module',
+        );
+        const edgeId = depNode.parentPath + EDGE_PATH_SEPARATOR + depNode.childPath;
+        edgeDict[edgeId] = createEdge(edgeId);
       } else {
         const specifierName = specifier.importedName ?? specifier.localName;
         const nodeId = depNode.parentPath + NODE_PATH_SEPARATOR + specifierName;
-        specifierSet.add(nodeId);
-        edgesSet.add(depNode.parentPath + EDGE_PATH_SEPARATOR + nodeId);
+        nodesDict[nodeId] = createNode(
+          nodeId,
+          specifierName,
+          'specifier',
+        );
+        const edgeId = depNode.parentPath + EDGE_PATH_SEPARATOR + nodeId;
+        edgeDict[edgeId] = createEdge(edgeId);
       }
     });
   });
 
-  const components = Array.from(componentsSet).map((nodePath) => ({
-    data: {
-      id: nodePath,
-      name: nodePath.split('/').at(-1),
-      type: 'module',
-    }
-  }));
-
-  const specifiers = Array.from(specifierSet).map((nodePath) => ({
-    data: {
-      id: nodePath,
-      name: nodePath.split(NODE_PATH_SEPARATOR).at(-1),
-      type: 'specifier',
-    }
-  }));
-  
-  const edges = Array.from(edgesSet).map((edgePath) => {
-    const [source, target] = edgePath.split(EDGE_PATH_SEPARATOR);
-    return ({
-      data: {
-        id: edgePath,
-        source,
-        target,
-      },
-    })
-  });
-
   return {
-    nodes: [...components, ...specifiers],
-    edges,
+    nodes: Object.values(nodesDict),
+    edges: Object.values(edgeDict),
   }
 }
